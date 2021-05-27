6e29512ed023086ab4e59e1fc81e21a06b7c5d6a
aryantandale <84887052+aryantandale@users.noreply.github.com>
2021-05-26 22:23:33 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/********************************************************************************************* Java Program Name 
+********************************************************************************************** Aryan Tandale 
+* May 26, 2021 
+* CMSC 255 Section C01 
+*********************************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Aryan Tandale>"); 
+		System.out.println("<Hello.java>");
+		System.out.println("<Hello.java program part of Lab 01>");
+		System.out.println("<May 26, 2021>");
+		System.out.println("<CMSC 255 Section C01>");
+		System.out.println();
+
+	}
+}
